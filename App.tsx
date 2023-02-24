3e0076183f2a7c08e1bca318c08c46afcda5c88c
Lucas <lucas@hyperhire.in>
2023-02-24 18:54:19 +0900

    modify: init after check available module
    모듈 사용가능 여부 확인 후 초기화

--- App.tsx
+++ App.tsx
@@ -23,74 +23,77 @@ function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const endTime = new Date().toISOString();
   const connectHealthModule = async () => {
-    console.log('start initialize');
-    const isInitialized = await initialize();
-    console.log('Initialization Result: ', isInitialized);
-
     console.log('start check available');
     const check = await checkHealthConnectSupported();
     console.log('check available result: ', check);
+    if (check.healthPlatformInstalled && check.isApiSupported) {
+      console.log('start initialize');
+      const isInitialized = await initialize();
+      console.log('Initialization Result: ', isInitialized);
+      if (isInitialized) {
+        console.log('start  requestPermissions');
+        const grantedPermissions = await requestPermissions([
+          {accessType: 'read', recordType: 'HeartRate'},
+          {accessType: 'read', recordType: 'RestingHeartRate'},
+          {accessType: 'read', recordType: 'SleepSession'},
+          {accessType: 'read', recordType: 'SleepStage'},
+          {accessType: 'read', recordType: 'Steps'},
+        ]);
+        console.log('grantedPermissions result: ', grantedPermissions);
 
-    console.log('start  requestPermissions');
-    const grantedPermissions = await requestPermissions([
-      {accessType: 'read', recordType: 'HeartRate'},
-      {accessType: 'read', recordType: 'RestingHeartRate'},
-      {accessType: 'read', recordType: 'SleepSession'},
-      {accessType: 'read', recordType: 'SleepStage'},
-      {accessType: 'read', recordType: 'Steps'},
-    ]);
-    console.log('grantedPermissions result: ', grantedPermissions);
+        console.log('start get sample data');
+        const heartRate = await readRecords('HeartRate', {
+          timeRangeFilter: {
+            operator: 'between',
+            startTime: '2023-01-09T12:00:00.405Z',
+            endTime: endTime,
+          },
+        });
+        console.log('heartRateRec', heartRate);
 
-    console.log('start get sample data');
-    const heartRate = await readRecords('HeartRate', {
-      timeRangeFilter: {
-        operator: 'between',
-        startTime: '2023-01-09T12:00:00.405Z',
-        endTime: endTime,
-      },
-    });
-    console.log('heartRateRec', heartRate);
+        const restingHeartRate = await readRecords('RestingHeartRate', {
+          timeRangeFilter: {
+            operator: 'between',
+            startTime: '2023-01-09T12:00:00.405Z',
+            endTime: endTime,
+          },
+        });
 
-    const restingHeartRate = await readRecords('RestingHeartRate', {
-      timeRangeFilter: {
-        operator: 'between',
-        startTime: '2023-01-09T12:00:00.405Z',
-        endTime: endTime,
-      },
-    });
+        console.log('restingHeartRate', restingHeartRate);
 
-    console.log('restingHeartRate', restingHeartRate);
+        const steps = await readRecords('Steps', {
+          timeRangeFilter: {
+            operator: 'between',
+            startTime: '2023-01-09T12:00:00.405Z',
+            endTime: endTime,
+          },
+        });
 
-    const steps = await readRecords('Steps', {
-      timeRangeFilter: {
-        operator: 'between',
-        startTime: '2023-01-09T12:00:00.405Z',
-        endTime: endTime,
-      },
-    });
+        console.log('Steps', steps);
 
-    console.log('Steps', steps);
+        const sleepSession = await readRecords('SleepSession', {
+          timeRangeFilter: {
+            operator: 'between',
+            startTime: '2023-01-09T12:00:00.405Z',
+            endTime: endTime,
+          },
+        });
 
-    const sleepSession = await readRecords('SleepSession', {
-      timeRangeFilter: {
-        operator: 'between',
-        startTime: '2023-01-09T12:00:00.405Z',
-        endTime: endTime,
-      },
-    });
+        console.log('sleepSession', sleepSession);
 
-    console.log('sleepSession', sleepSession);
+        const sleepStage = await readRecords('SleepStage', {
+          timeRangeFilter: {
+            operator: 'between',
+            startTime: '2023-01-09T12:00:00.405Z',
+            endTime: endTime,
+          },
+        });
 
-    const sleepStage = await readRecords('SleepStage', {
-      timeRangeFilter: {
-        operator: 'between',
-        startTime: '2023-01-09T12:00:00.405Z',
-        endTime: endTime,
-      },
-    });
+        console.log('sleepStage', sleepStage);
+      };
+    }
+  }
 
-    console.log('sleepStage', sleepStage);
-  };
 
   useEffect(() => {
     connectHealthModule().then(() => console.log('finish get sample data'));
